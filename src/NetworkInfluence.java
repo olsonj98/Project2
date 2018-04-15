67a747f61988977fe020b40a40fedc6f1fd67a21
olsonj98 <olsonj98@gmail.com>
2018-04-15 17:35:33 -0500

    updated networkInfluence
    added last semester's code to network influence, definitely some changes here between then and now

--- src/NetworkInfluence.java
+++ src/NetworkInfluence.java
@@ -8,24 +8,75 @@
 //  (i.e., you may only include libraries of the form java.*)
 
 /**
-* @author Hugh Potter
+* @authors Jeff, Kevin, Tyler
 */
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class NetworkInfluence
 {
+	public ArrayList<Vertex> graph = new ArrayList<Vertex>();
+	public int numVertices;
+	
 	// NOTE: graphData is an absolute file path that contains graph data, NOT the raw graph data itself
 	public NetworkInfluence(String graphData)
 	{
-		// implementation
+		File file = new File(graphData);
+		Scanner sc;
+		boolean firstFound, secondFound;
+		Vertex first = new Vertex();
+		Vertex second = new Vertex();
+		try {
+			sc = new Scanner(file);
+			if(sc.hasNextLine()) numVertices = sc.nextInt();
+			
+			while(sc.hasNextLine()){
+				String[] edge = {sc.next(), sc.next()};
+				
+				firstFound = false;
+				secondFound = false;
+				for(Vertex v : graph){
+					if(v.data.equals(edge[0])){
+						firstFound = true;
+						first = v;
+					}
+					if(v.data.equals(edge[1])){
+						secondFound = true;
+						second = v;
+					}
+				}
+				
+				if(!firstFound){
+					first = new Vertex(edge[0]);
+					graph.add(first);
+				}
+				
+				if(!secondFound){
+					second = new Vertex(edge[1]);
+					graph.add(second);
+				}
+				
+				first.to.add(second);
+				second.from.add(first);
+			}
+			
+			sc.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 
 	public int outDegree(String v)
 	{
-		// implementation
+		for(Vertex vertex : graph){
+			if(vertex.data.equals(v)){
+				return vertex.to.size();
+			}
+		}
 
-		// replace this:
 		return -1;
 	}
 
@@ -91,5 +142,19 @@ public class NetworkInfluence
 
 		// replace this:
 		return null;
+	}
+	
+	private class Vertex{
+		ArrayList<Vertex> to = new ArrayList<Vertex>();
+		ArrayList<Vertex> from = new ArrayList<Vertex>();
+		String data;
+		
+		public Vertex(){
+			data = "";
+		}
+		
+		public Vertex(String str){
+			data = str;
+		}
 	}
 }
