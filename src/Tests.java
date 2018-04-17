d22ae9b4291f73936cd55c46638c3b538f8303f3
olsonj98 <olsonj98@gmail.com>
2018-04-16 19:09:41 -0500

    updated test file
    I think wiki test works how it should, but network test breaks

--- src/Tests.java
+++ src/Tests.java
@@ -3,11 +3,25 @@ import java.util.ArrayList;
 
 public class Tests {
 	public static void main(String[] args) throws Exception {
+		//testWikiCrawler();
+		testNetworkInfluence();
+	}
+	
+	private static void testWikiCrawler(){
 		ArrayList<String> topics = new ArrayList<String>();
 		topics.add("length");
 		topics.add("height");
 		topics.add("width");
 		WikiCrawler w = new WikiCrawler("/wiki/Size", 10, topics, "WikiISU.txt");
 		w.crawl();
+	}
+	
+	private static void testNetworkInfluence(){
+		NetworkInfluence graph = new NetworkInfluence("wikiCC.txt");
+		String u = "/wiki/Complexity_theory";
+		String v = "/wiki/System";
+		String inf = "/wiki/Complexity";
+		System.out.println("Distance between  " + u + " and " + v + ": " + graph.distance(u, v));
+		System.out.println("Influence of " + inf + ": " + graph.influence(inf));
 	}
 }
