71e12aaed1949f6cdee705a746ac67dbf4fb8d60
olsonj98 <olsonj98@gmail.com>
2018-04-19 19:45:33 -0500

    updated vertex class
    new parent variable and new constructor

--- src/Vertex.java
+++ src/Vertex.java
@@ -1,15 +1,19 @@
-import java.util.ArrayList;
-
 public class Vertex {
-
-	public ArrayList<Vertex> to = new ArrayList<Vertex>();
 	public String data;
+	public Vertex parent;
 	
 	public Vertex(){
 		this.data = "";
+		this.parent = null;
 	}
 	
 	public Vertex(String url){
 		this.data = url;
+		this.parent = null;
+	}
+	
+	public Vertex(String url, Vertex p){
+		this.data = url;
+		this.parent = p;
 	}
 }
